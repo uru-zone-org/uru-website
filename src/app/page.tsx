b5868d622fdee739f6b6697b993f55b8e0c58f6f
Sigrídur Birna Matthíasdóttir <sigridurbirnamatthiasdottir@Sigridurs-MacBook-Pro.local>
2025-07-11 13:36:35 +0000

    laga hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,12 @@ export default function HomePage() {
   return (
     <section className="flex items-center justify-center h-[60vh] px-8 max-w-3xl mx-auto">
       <div className="space-y-4 text-center">
-        <blockquote className="typography-body leading-tight">
+        <blockquote className="typography-h2">
           “URU.zone brings together
           <br />wearables, software, and sensory design
           <br />transforming workouts into
           <br />intelligent, data-driven experiences.”
         </blockquote>
-        <p className="typography-body">The technology is ready.</p>
-        <p className="typography-body">URU.Wear soft launch begins early 2026.</p>
-        <p className="typography-body">Join the Zone.</p>
       </div>
     </section>
   );
